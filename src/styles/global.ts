091d65b478683af9ca3df982c5b724670c35794f
KelviPascoal <kelvi_ugo@hotmail.com>
2025-07-16 13:40:14 -0300

    chore: configure global styles

---
+++ src/styles/global.ts
@@ -0,0 +1,56 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+  /* Reset básico */
+  *, *::before, *::after {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  /* Estilo base da página */
+  html, body {
+    font-family: ${({ theme }) => theme.fonts.main};
+    background-color: ${({ theme }) => theme.colors.background};
+    color: ${({ theme }) => theme.colors.text};
+    line-height: 1.6;
+    min-height: 100%;
+    -webkit-font-smoothing: antialiased;
+  }
+
+  a {
+    color: ${({ theme }) => theme.colors.primary};
+    text-decoration: none;
+  }
+
+  a:hover {
+    text-decoration: underline;
+  }
+
+  h1, h2, h3, h4, h5, h6 {
+    font-family: ${({ theme }) => theme.fonts.heading};
+    margin-bottom: ${({ theme }) => theme.spacing.medium};
+    color: ${({ theme }) => theme.colors.primary};
+  }
+
+  p {
+    margin-bottom: ${({ theme }) => theme.spacing.medium};
+  }
+
+  ul {
+    list-style: none;
+    padding-left: 0;
+  }
+
+  button {
+    font-family: inherit;
+    cursor: pointer;
+    background: none;
+    border: none;
+  }
+
+  img {
+    max-width: 100%;
+    display: block;
+  }
+`;
---
+++ src/styles/styled.d.ts
@@ -0,0 +1,23 @@
+// styled.d.ts
+import 'styled-components';
+
+declare module 'styled-components' {
+    export interface DefaultTheme {
+        colors: {
+            primary: string;
+            accent: string;
+            background: string;
+            text: string;
+            border: string;
+        };
+        fonts: {
+            main: string;
+            heading: string;
+        };
+        spacing: {
+            small: string;
+            medium: string;
+            large: string;
+        };
+    }
+}
